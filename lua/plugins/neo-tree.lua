3464ce61b96d9d36df1cb7be32f95cb3e1d69866
irsyadhsn <irsyadhs2003@gmail.com>
2024-12-25 14:17:54 +0700

    more view changes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,103 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				follow_current_file = { enabled = true },
-			},
-		})
-		vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>", {})
-	end,
+   "nvim-neo-tree/neo-tree.nvim",
+   branch = "v3.x",
+   dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+   },
+   config = function()
+      require("neo-tree").setup({
+         close_if_last_window = false,
+         filesystem = {
+            follow_current_file = { enabled = true },
+         },
+         default_component_configs = {
+            indent = {
+               indent_size = 2,
+               padding = 1, -- extra padding on left hand side
+               -- indent guides
+               with_markers = true,
+               indent_marker = "│",
+               last_indent_marker = "└",
+               highlight = "NeoTreeIndentMarker",
+               -- expander config, needed for nesting files
+               with_expanders = "enabled", -- if nil and file nesting is enabled, will enable expanders
+               expander_collapsed = "",
+               expander_expanded = "",
+               expander_highlight = "NeoTreeExpander",
+            },
+            icon = {
+               folder_closed = "󰉋",
+               folder_open = "",
+               folder_empty = "󰜌",
+               provider = function(icon, node) -- default icon provider utilizes nvim-web-devicons if available
+                  if node.type == "file" or node.type == "terminal" then
+                     local success, web_devicons = pcall(require, "nvim-web-devicons")
+                     local name = node.type == "terminal" and "terminal" or node.name
+                     if success then
+                        local devicon, hl = web_devicons.get_icon(name)
+                        icon.text = devicon or icon.text
+                        icon.highlight = hl or icon.highlight
+                     end
+                  end
+               end,
+               default = "",
+               highlight = "NeoTreeFileIcon",
+            },
+            modified = {
+               symbol = "",
+               highlight = "NeoTreeModified",
+            },
+            name = {
+               trailing_slash = false,
+               use_git_status_colors = true,
+               highlight = "NeoTreeFileName",
+            },
+            git_status = {
+               symbols = {
+                  -- Change type
+                  added = "+", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                  modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                  deleted = "✖", -- this can only be used in the git_status source
+                  renamed = "󰁕", -- this can only be used in the git_status source
+                  -- Status type
+                  untracked = " ",
+                  ignored = "",
+                  unstaged = "󰄱",
+                  staged = "",
+                  conflict = "",
+               },
+            },
+            -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
+            file_size = {
+               enabled = true,
+               width = 12, -- width of the column
+               required_width = 64, -- min width of window required to show this column
+            },
+            type = {
+               enabled = true,
+               width = 10, -- width of the column
+               required_width = 122, -- min width of window required to show this column
+            },
+            last_modified = {
+               enabled = true,
+               width = 20, -- width of the column
+               required_width = 88, -- min width of window required to show this column
+            },
+            created = {
+               enabled = true,
+               width = 20, -- width of the column
+               required_width = 110, -- min width of window required to show this column
+            },
+            symlink_target = {
+               enabled = false,
+            },
+         },
+      })
+      vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#BFECFF" })
+      vim.api.nvim_set_hl(0, "NeoTreeGitUnstaged", { fg = "#C5D3E8" })
+      vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#C9E9D2" })
+      vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = "#FFB0B0" })
+      vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>", {})
+   end,
 }
--- lua/vim-opt.lua
+++ lua/vim-opt.lua
@@ -1,7 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=3")
+vim.cmd("set softtabstop=3")
+vim.cmd("set shiftwidth=3")
 --vim.cmd("set smartindent")
 
 vim.opt.showmode = false
@@ -14,13 +14,20 @@ vim.wo.number = true
 vim.opt.relativenumber = true
 vim.api.nvim_create_autocmd("InsertEnter", { command = [[set norelativenumber]] })
 vim.api.nvim_create_autocmd("InsertLeave", {
-    callback = function()
-        -- Check if current buffer is not a Neo-tree buffer
-        if vim.bo.filetype ~= 'neo-tree' then
-            vim.cmd('set relativenumber')
-        end
-    end
+   callback = function()
+      -- Check if current buffer is not a Neo-tree buffer
+      if vim.bo.filetype ~= "neo-tree" then
+         vim.cmd("set relativenumber")
+      end
+   end,
 })
 -- vim.api.nvim_create_autocmd("InsertLeave", { command = [[set relativenumber]] })
 
 vim.opt.swapfile = false
+-- Set options
+vim.opt.fillchars = {
+  vert = ' ',
+}
+
+-- Set highlight groups
+vim.api.nvim_set_hl(0, "VertSplit", { bg = "NONE", fg = "NONE" })
